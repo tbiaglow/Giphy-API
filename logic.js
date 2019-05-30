5e4228e8b5e4764e8c438d422f3270dd04de4d05
tbiaglow <tbiaglow@gmail.com>
2019-05-29 20:46:19 -0400

    starting buttons now work

--- logic.js
+++ logic.js
@@ -1,20 +1,23 @@
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    topics = ["Pikachu", "Eevee", "Meowth", "Bulbasaur", "Charmander", "Squirtle", "Snorlax", "Psyduck", "Dugtrio", "Togepi"];
+topics = ["Pikachu", "Eevee", "Meowth", "Bulbasaur", "Charmander", "Squirtle", "Snorlax", "Psyduck", "Dugtrio", "Togepi"];
 
 topics.forEach(createHTML)
 
+$(".pokebutton").on("click", function(){getData($(this).attr("id"))});
+
+
 function createHTML(pokemon) {
-    $('.pokebuttons').append("<button id = " + pokemon + ">" + pokemon + "</button>");
-    // $('#' + pokemon).on("click", getData(pokemon));
+    $('.pokebuttons').append("<button class='pokebutton' id = " + pokemon + ">" + pokemon + "</button>");
 }
 
 function getData(input) {
+    console.log(input)
 
     // var input = $('#searchtext').val()
 
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=wmjmmD2VxQQQQHscbOOsoXe29b7p2xaN&limit=10");
+    var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=wmjmmD2VxQQQQHscbOOsoXe29b7p2xaN&limit=10");
 
 
     xhr.done(function(response) { 
@@ -24,11 +27,12 @@ function getData(input) {
         var gifs = response.data
 
         for (i = 0; i < gifs.length; i++) {
-            $('.inner').append("<img src='"+gifs[i].images.original.url+"'/>")
+            $('.inner').prepend("<img src='"+gifs[i].images.original.url+"'/>")
         }
 
     });
 
 }
 
-});
+
+// });
