bd8b84474416a58e95df26e3a3ce5ee646899641
tbiaglow <tbiaglow@gmail.com>
2019-05-31 06:37:29 -0400

    gifs now stop and start on click! Finally!

--- logic.js
+++ logic.js
@@ -1,6 +1,4 @@
 
-// $(document).ready(function(){
-
 topics = ["Pikachu", "Eevee", "Meowth", "Bulbasaur", "Charmander", "Squirtle", "Snorlax", "Psyduck", "Dugtrio", "Togepi"];
 
 topics.forEach(createHTML)
@@ -19,49 +17,8 @@ function createHTML(pokemon) {
 }
 
 function getData(input) {
-    // console.log(input)
-
-    // // var input = $('#searchtext').val()
-
-    // var xhr = $.get("https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=wmjmmD2VxQQQQHscbOOsoXe29b7p2xaN&limit=10");
-
-
-    // xhr.done(function(response) { 
-
-    //     console.log("success got data", response); 
-
-    //     var gifs = response.data
-    //     for (i = 0; i < gifs.length; i++) {
-    //         $('.inner').prepend("<img class='pokegif' src='"+gifs[i].images.fixed_height_still.url+"' data-state='still'/>")
-    //         var URL_still = "gifs[i].images.fixed_height_still.url";
-    //         var URL_animate = "gifs[i].images.original.url"
-    //         $(".pokegif").on("click", function() {
-
-    //             var state = $(this).attr("data-state");
-    
-    //             if (state === "still") {
-    //             //   $(this).attr("src", $(this).attr("data-animate"));
-    //               $(this).attr("src", URL_still)
-    //               $(this).attr("data-state", "animate");
-    //             } else {
-    //             //   $(this).attr("src", $(this).attr("data-still"));
-    //               $(this).attr("src=", URL_animate)
-    //               $(this).attr("data-state", "still");
-    //             }
-    //           });
-    //     }
-
-    //     return gifs[1]
-
-
-      
-
-    // });
-
-
 
     var pokemon = input;
-
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       pokemon + "&api_key=wmjmmD2VxQQQQHscbOOsoXe29b7p2xaN&limit=10";
@@ -70,9 +27,7 @@ function getData(input) {
     $.ajax({
       url: queryURL,
       method: "GET"
-    })
-
-      .then(function(response) {
+    }).then(function(response) {
         console.log(queryURL);
 
         console.log(response);
@@ -92,18 +47,21 @@ function getData(input) {
           var pokemonImage = $("<img>");
 
           pokemonImage.attr("src", results[i].images.fixed_height_still.url);
+          pokemonImage.attr("data-still", results[i].images.fixed_height_still.url)
+          pokemonImage.attr("data-animate", results[i].images.fixed_height.url)
+          //Supposed to change state between still and animated, but it doesn't work yet.
           pokemonImage.attr("data-state", "still")
           pokemonImage.on("click", function() {
 
             var state = $(this).attr("data-state");
 
             if (state === "still") {
-            //   $(this).attr("src", $(this).attr("data-animate"));
-              $(this).attr("src", results[i].images.fixed_height.url)
+              $(this).attr("src", $(this).attr("data-animate"));
+            //   $(this).attr("src", results[i].images.fixed_height.url)
               $(this).attr("data-state", "animate");
             } else {
-            //   $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("src", results[i].images.fixed_height_still.url)
+              $(this).attr("src", $(this).attr("data-still"));
+            //   $(this).attr("src", results[i].images.fixed_height_still.url)
               $(this).attr("data-state", "still");
             }
           });
@@ -117,5 +75,3 @@ function getData(input) {
 
 }
 
-
-// });
